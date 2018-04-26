ecf4eced2b1b388ba7362d8a4841cb21dea38dbe
liaojianbin <meinitcasthavefun@foxmail.com>
2018-04-26 10:18:19 +0800

    mybatis分页助手PageHelper

--- itcast-mybatis/src/test/java/cn/itcast/mybatis/mapper/StudentMapperTest.java
+++ itcast-mybatis/src/test/java/cn/itcast/mybatis/mapper/StudentMapperTest.java
@@ -1,6 +1,8 @@
 package cn.itcast.mybatis.mapper;
 
 import cn.itcast.mybatis.pojo.Student;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -37,6 +39,24 @@ public class StudentMapperTest {
     public void queryAllStudents() {
         List<Student> list = studentMapper.queryAllStudents();
         for (Student student : list) {
+            System.out.println(student);
+        }
+    }
+
+    @Test
+    public void queryAllStudentsInPage() {
+        //设置分页:第一个参数：页号，第二个参数：页大小；只对紧接着的查询语句生效
+        PageHelper.startPage(2,2);
+
+        List<Student> list = studentMapper.queryAllStudents();
+
+        //构造分页信息对象
+        PageInfo<Student> pageInfo = new PageInfo<>(list);
+
+        System.out.println("总记录数为：" + pageInfo.getTotal() + "；总页数为：" + pageInfo.getPages() +
+                "；当前页号为：" + pageInfo.getPageNum() + "；页大小为：" + pageInfo.getPageSize());
+
+        for (Student student : pageInfo.getList()) {
             System.out.println(student);
         }
     }
